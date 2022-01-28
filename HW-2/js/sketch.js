ff5182880cabba459803945d1c6c5fcd3becd624
Dani-Briggs <42754006+Dani-Briggs@users.noreply.github.com>
2022-01-28 13:08:22 -0700

    fix

--- HW-2/js/sketch.js
+++ HW-2/js/sketch.js
@@ -7,23 +7,10 @@ function setup() {
 
 function draw() {
   background(200);
-
-
   //text
   text('Homework 2', 10, 10);
-  text('Dani Briggs', 730, 500);
+  text('Dani Briggs', 730, 490);
   //circle
-  //square
-   square(100,40,50);
-
-   //rect
-   rect(100,20,50,60);
-
-   circle(100,10,300);
-
- //square
-  square(100,0,50);
-
-  //rect
-  rect(100,0,50,60);
+  circle(20,20,40);
+  
 }

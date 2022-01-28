53cf8d843f79ad915f863742441ee5ae933467af
Dani-Briggs <42754006+Dani-Briggs@users.noreply.github.com>
2022-01-28 12:03:45 -0700

    shapes added

---
+++ HW-2/js/sketch.js
@@ -0,0 +1,17 @@
+//setting up
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(220);
+
+  //circle
+  circle(30,40,50);
+
+ //square
+  square(30,40,50);
+
+  //rect
+  rect(30,40,50,60);
+}

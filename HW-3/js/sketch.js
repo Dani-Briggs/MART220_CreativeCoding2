00b90a10f4dd55809f5fbe6a0c374e8538e6adfb
Dani-Briggs <42754006+Dani-Briggs@users.noreply.github.com>
2022-01-31 14:10:45 -0700

    started hw3

---
+++ HW-3/js/sketch.js
@@ -0,0 +1,111 @@
+//vars
+var x = 0;
+var y = 100;
+var size = 25;
+var speedX = 0;
+var speedY = 0;
+var speedMX = 0;
+var speedMY = 0;
+var r = 0;
+var g = 0;
+var b = 0;
+var mX = 100;
+var mY = 25;
+var rectX = 0;
+var rectY = 0;
+//setting up
+function setup() {
+  createCanvas(800, 500);
+  speedX = random(1,10);
+  speedY = random(1,10);
+}
+
+function draw() {
+  background(200);
+  fill(r,g,b);
+  //text
+  text('Homework 2', 10, 21);
+  text('Dani Briggs', 650, 490);
+
+  //Square
+  fill(100,100,29);
+  square(mX, mY, 60);
+
+
+  //circle
+  fill(150,60,29);
+  circle(x, y, size);
+  //if statements for circle
+  if (x >= 800){
+    speedX = (1,10);
+    speedX = -speedX;
+  }
+  else if (x<0) {
+    speedX = random(1,10);
+  }
+  else if (y >= 500){
+    speedY = random(1,10);
+    speedY = -speedY;
+  }
+  else if (y<0) {
+    speedY = random(1,10);
+  }
+  ////////////
+  //if statements for square
+  if (x >= 800){
+    speedMX = (1,10);
+    speedMX = -speedMX;
+  }
+  else if (x<0) {
+    speedMX = random(1,10);
+  }
+  else if (y >= 500){
+    speedMY = random(1,10);
+    speedMY = -speedMY;
+  }
+  else if (y<0) {
+    speedMY = random(1,10);
+  }
+
+  //for circle
+  x = x + speedX;
+  y = y + speedY;
+  //for Square
+  mX = mY + speedX;
+  mY = mX + speedY;
+
+  moveRect();
+
+  backButton();
+}
+
+function moveRect(){
+  //rect will move with wasd
+  //rect
+  fill(20,100,29);
+  rect(320, 420, 40, 70);
+  if(keyIsPressed){
+    if(key == 'a'){
+      rectX=-5;
+    }
+    else if (key == 'd') {
+      rectX=+5;
+    }
+    else if (key == 'w') {
+      rectY=-5;
+    }
+    else if (key == 's') {
+      rectY=+5;
+    }
+  }
+}
+
+
+//backbutton
+function backButton(){
+  fill(35, 22, 180);
+  rect(12, 450, 70, 40);
+  fill(r, g, b);
+  textSize(25);
+  text('Back', 19, 475);
+}
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # MART220_CreativeCoding2
 ## Hi I'm Dani!
 [HW 2](https://dani-briggs.github.io/MART220_CreativeCoding2/HW-2/)
+[HW 2](https://dani-briggs.github.io/MART220_CreativeCoding2/HW-3/)
